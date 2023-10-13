11e43a3aa9d03c1079b400dd67799d9040e5ed88
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-14 00:49:14 +0900

    lecture: react flagment lecture

---
+++ 04_react_basic/src/073_practice_component/start/components/Child.js
@@ -0,0 +1,9 @@
+const Child = () => {
+  return (
+    <div className="component">
+      <h3>Child component</h3>
+    </div>
+  );
+};
+
+export default Child;
--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,26 @@
-import "./Child.css";
+import './Child.css';
+import { Fragment } from 'react';
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    // 最上位のタグは1つである必要がある
+    // React.Fragmentで全てのタグをまとめてあげる(フラグメントタグ自体は表示されない)
+    // 余計なタグを出したくない場合に便利
+    // ReactはFragment以外にも様々な関数・コンポーネントが標準実装されている
+    // Fragmentの代わりに<></>でもOK（import不要）
+    // 唯一key属性だけFragmentに指定可能（その場合はFlagmentは省略不可）
+    <Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Flagment</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+        nesciunt omnis iste, obcaecati accusamus aperiam. Nostrum magnam non
+        placeat fugiat odio eos, blanditiis voluptatibus asperiores quod. Illum
+        rerum modi non?
+      </p>
+    </Fragment>
   );
 };
 
