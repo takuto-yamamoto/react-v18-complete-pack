1c90a066261c44fc89c00e4709c063e192dc6922
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-14 01:05:17 +0900

    lecture: expr_in_jsx

---
+++ 04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,25 @@
+import './Expression.css';
+
+const Expression = () => {
+  const title = 'Expression';
+  const array = ['item1', 'item2', 'item3'];
+  const hello = (arg) => {
+    `${arg} function`;
+  };
+  const jsx = <h3>Hello JSX</h3>;
+  const bool = true;
+  // プレースホルダ内にJS式を格納可能
+  // 配列は自動で展開される
+  return (
+    <div className={title.toLowerCase()}>
+      <h3>Hello {title}</h3>
+      <h3>{array}</h3>
+      <h3>{hello('hello')}</h3>
+      <h3>{/**画面上に表示されない */}</h3>
+      {jsx}
+      {bool /**何も出力されない */}
+    </div>
+  );
+};
+
+export default Expression;
