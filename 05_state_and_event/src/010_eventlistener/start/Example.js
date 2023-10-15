5cacfdee80c2e3ab4c8a53864dd6bf29df3f34ea
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-16 00:42:22 +0900

    lecture: event listener and some event types.

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,17 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('ボタンがクリックされました');
+  };
+  const clickHandler2 = () => {
+    console.log('ボタンがクリックされました');
+  };
   return (
-    <></>
+    <>
+      {/** HTMLならonclickだがJSだとonClick*/}
+      {/** onClickに渡すのは関数自体で、関数実行した返り値ではない */}
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button onClick={clickHandler2}>クリックしてね</button>
+    </>
   );
 };
 
--- 05_state_and_event/src/015_other_events/start/Example.js
+++ 05_state_and_event/src/015_other_events/start/Example.js
@@ -1,4 +1,4 @@
-import "./Example.css";
+import './Example.css';
 
 const Example = () => {
   return (
@@ -8,24 +8,29 @@ const Example = () => {
         入力値のイベント：
         <input
           type="text"
-          onChange={() => console.log("onChange検知")}
-          onBlur={() => console.log("onBlur検知")}
-          onFocus={() => console.log("onFocus検知")}
+          // 文字列が変わったとき
+          onChange={() => console.log('onChange検知')}
+          // 入力欄がフォーカスを失った時
+          onBlur={() => console.log('onBlur検知')}
+          // フォーカスされた時
+          onFocus={() => console.log('onFocus検知')}
         />
       </label>
-      {/* <div>
+      <div>
         <label>
           入力値を取得：
+          {/** event.target.valueで取得可能 */}
           <input type="text" onChange={(e) => console.log(e.target.value)} />
         </label>
-      </div> */}
-      {/* <div
+      </div>
+      * {/** マウスが領域に入った時、出て行った時 */}
+      <div
         className="hover-event"
-        onMouseEnter={() => console.log("カーソルが入ってきました。")}
-        onMouseLeave={() => console.log("カーソルが出ていきました。")}
+        onMouseEnter={() => console.log('カーソルが入ってきました。')}
+        onMouseLeave={() => console.log('カーソルが出ていきました。')}
       >
         ホバーしてね！
-      </div> */}
+      </div>
     </div>
   );
 };
