273a12a1ded70ca5861df849ef1004969063e3b6
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-28 00:34:40 +0900

    lecture: redux async func

--- 13_redux/start/src/060_createAsyncThunk/store/modules/counter.js
+++ 13_redux/start/src/060_createAsyncThunk/store/modules/counter.js
@@ -1,11 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-import { asyncCount } from "../../api/counter"
+import { asyncCount } from '../../api/counter';
 
 const counter = createSlice({
   name: 'counter',
   initialState: {
-    count: 0
+    count: 0,
+    status: '',
   },
   reducers: {
     add(state, { type, payload }) {
@@ -19,11 +20,32 @@ const counter = createSlice({
       // const newState = { ...state };
       // newState.count = state.count - payload
       // return newState;
-    }
-  }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(addAsyncWithStatus.pending, (state) => {
+        state.status = 'Loading...';
+      })
+      .addCase(addAsyncWithStatus.fulfilled, (state, action) => {
+        state.status = '取得済み';
+        state.count += action.payload;
+      })
+      .addCase(addAsyncWithStatus.rejected, (state) => {
+        state.status = 'エラー';
+      });
+  },
 });
 
 const { add, minus } = counter.actions;
+
+const addAsyncWithStatus = createAsyncThunk(
+  'counter/asyncCount',
+  async (payload) => {
+    const response = await asyncCount(payload);
+    return response.data;
+  }
+);
 
 const addAsync = (payload) => {
   return async (dispatch, getState) => {
@@ -31,9 +53,8 @@ const addAsync = (payload) => {
     console.log(state);
     const response = await asyncCount(payload);
     dispatch(add(response.data));
-  }
-}
+  };
+};
 
-
-export { add, minus, addAsync }
-export default counter.reducer
+export { add, minus, addAsync, addAsyncWithStatus };
+export default counter.reducer;
