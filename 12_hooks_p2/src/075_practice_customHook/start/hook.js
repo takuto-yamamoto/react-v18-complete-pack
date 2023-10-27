1f9c4d3c535af6b14b028aad2ae3273ab641e0fd
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-27 20:24:32 +0900

    practice: custom hook

---
+++ 12_hooks_p2/src/075_practice_customHook/start/hook.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export const useCount = () => {
+  const [count, setCount] = useState(0);
+  const countUp = () => setCount((prev) => prev + 1);
+
+  return {
+    count,
+    countUp,
+  };
+};
